3a2fcb0f1fc4c4fb0fd43df5d8b5fe8ad4488796
Imam <imamtrywibowo257@gmail.com>
2021-06-09 21:09:19 +0700

    add delete & issue book

--- library-apps/ViewBooks.py
+++ library-apps/ViewBooks.py
@@ -9,36 +9,45 @@ DB = 'library-db'
 conn = pymysql.connect(host='localhost', user="root", database=DB, password=pwd)
 cur = conn.cursor()
 
-# def view():
-root = Tk()
-root.title('Library')
-root.minsize(width=800, height=600)
-root.geometry('800x600')
+def view():
+    root = Tk()
+    root.title('Library')
+    root.minsize(width=800, height=600)
+    root.geometry('800x600')
 
-canvas2 = Canvas(root)
-canvas2.config(bg="green")
-canvas2.pack(expand=True, fill=BOTH)
+    canvas2 = Canvas(root)
+    canvas2.config(bg="green")
+    canvas2.pack(expand=True, fill=BOTH)
 
-# content heading 
-headingFrame = Frame(root, bg='blue', bd=5)
-headingFrame.place(relx=0.25, rely=0.2, relwidth=0.5, relheight=0.1)
+    # content heading 
+    headingFrame = Frame(root, bg='blue', bd=5)
+    headingFrame.place(relx=0.25, rely=0.2, relwidth=0.5, relheight=0.1)
 
-headingLabel = Label(headingFrame, text="View Books", bg='black', fg='white')
-headingLabel.place(relx=0.25, rely=0.25, relwidth=0.5, relheight=0.5)
+    headingLabel = Label(headingFrame, text="View Books", bg='black', fg='white')
+    headingLabel.place(relx=0.25, rely=0.25, relwidth=0.5, relheight=0.5)
 
-# content label list
-labelFrame = Frame(root, bg='black', bd=5)
-labelFrame.place(relx=0.1, rely=0.4, relwidth=0.8, relheight=0.5)
-Label(labelFrame, text="%-10s%-40s%-30s%-20s"%('BID','Title','Author','Status'), bg='black',fg='white').place(relx=0.07,rely=0.1)
-Label(labelFrame, text = "----------------------------------------------------------------------------",bg='black',fg='white').place (relx=0.05,rely=0.2)
+    # content label list
+    labelFrame = Frame(root, bg='black', bd=5)
+    labelFrame.place(relx=0.1, rely=0.4, relwidth=0.8, relheight=0.5)
+    Label(labelFrame, text="%-10s%-40s%-30s%-20s"%('BID','Title','Author','Status'), bg='black',fg='white').place(relx=0.07,rely=0.1)
+    Label(labelFrame, text = "----------------------------------------------------------------------------",bg='black',fg='white').place (relx=0.05,rely=0.2)
+    y = 0.25
 
-# getting data books
-bookTbl = 'books'
-getData = "select * from "+bookTbl
-cur.execute(getData)
-conn.commit()
+    # getting data books
+    bookTbl = 'books'
+    getData = "select * from "+bookTbl
 
-for i in cur:
-    print(i)
+    try:
+        cur.execute(getData)
+        conn.commit()
 
-root.mainloop()
+        for i in cur:
+            Label(labelFrame,text="%-10s%-30s%-30s%-20s"%(i[0],i[1],i[2],i[3]) ,bg='black', fg='white').place(relx=0.07,rely=y)
+            y += 0.1
+    except:
+        messagebox.showerror("Can't to View Book")
+
+    BtnExit = Button(root, text="Back to Home", bg="black", fg="white", command=root.destroy)
+    BtnExit.place(relx=0.4,rely=0.9, relwidth=0.18, relheight=0.08)
+
+    root.mainloop()
--- library-apps/index.py
+++ library-apps/index.py
@@ -4,7 +4,8 @@ import pymysql
 from tkinter import messagebox
 from AddBook import *
 from ViewBooks import *
-
+from DeleteBook import *
+from IssueBook import *
 
 # connect to db
 pwd = ''
@@ -29,13 +30,13 @@ headingLabel.place(relx=0, rely=0, relheight=1, relwidth=1)
 btnA = Button(root, text="Add Book", bg='black', fg='white', command=addBook)
 btnA.place(relx=0.1, rely=0.4, relwidth=0.4, relheight=0.1)
 
-btnB = Button(root, text="Delete Book", bg='black', fg='white')
+btnB = Button(root, text="Delete Book", bg='black', fg='white', command=delete)
 btnB.place(relx=0.1, rely=0.5, relwidth=0.4, relheight=0.1)
 
 btnC = Button(root, text="View Book List", bg='black', fg='white', command=view)
 btnC.place(relx=0.1, rely=0.6, relwidth=0.4, relheight=0.1)
 
-btnD = Button(root, text="Issue Book to Student", bg='black', fg='white')
+btnD = Button(root, text="Issue Book to Student", bg='black', fg='white', command=issueBook)
 btnD.place(relx=0.1, rely=0.7, relwidth=0.4, relheight=0.1)
 
 root.mainloop()
